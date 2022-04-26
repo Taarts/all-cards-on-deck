9b367fabe75bdf7be484a96b516c6ec1acdfbb3c
Tricia S Amheiser <tricia@taarts.net>
2022-04-25 23:43:19 -0400

    initial commit, doesn't like it from terminal

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace all_cards_on_deck
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("all-cards-on-deck");
+        }
     }
-  }
 }
